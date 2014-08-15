77ddb1b62d9a34184b90238dbe204a8b74137c97
Nokta-strigo <NoktaStrigo@yandex.ru>
2014-08-16 00:30:23 +0400

    New version with command-string arguments

--- site_pass_gen.py
+++ site_pass_gen.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 #__author__ = 'Nokta_strigo'
 
 import hashlib
@@ -11,9 +9,8 @@ import getpass
 
 chars = '`1234567890-=\\~!@#$%^&*()_+|qwertyuiop[]QWERTYUIOP{}asdfghjkl;\'ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>? '
 
-
 def convert_from_bin_to_string(bin_str):
-    """Generates a text password from bytes. The last char in password can be less random."""
+    """Generates a text password from bytes. The last chars in password are less random."""
     rand_l = 0
     for i in bin_str:
         rand_l = rand_l * 256 + i
@@ -23,13 +20,11 @@ def convert_from_bin_to_string(bin_str):
         rand_l //= len(chars)
     return s
 
-
 def ver2str(version):
     s = ""
     for i in range(version, -1, -1):
         s += str(i)
     return s
-
 
 def calc_site_pwd(version, site, login, master_password):
     """Generates site-specific password from the master password.
@@ -40,7 +35,7 @@ def calc_site_pwd(version, site, login, master_password):
 
     SitePwd = ripemd160(ver2str(version) + sha512_hex(ver2str(version) + master_password + site + login) + site)
     all strings are encoded in utf-8
-    where ver2str(version) returns (version+1) of consecutive decreasing numbers, for example:
+    where ver2str(version) returns (version+1) of consecutive decreasing numbers ending with 0, for example:
     ver2str(0) = 0
     ver2str(4) = 43210
     ver2str(12) = 1211109876543210
@@ -53,14 +48,10 @@ def calc_site_pwd(version, site, login, master_password):
     h2 = hashlib.new("ripemd160", h1.encode('utf-8')).digest()
     return convert_from_bin_to_string(h2)[:24]
 
-
-def get_master_password_fingerpring(master_password):
+def get_master_password_fingerprint(master_password):
     return hashlib.sha512(master_password.encode()).hexdigest()[:4]
 
-
-def generate_site_pwd():
-    master_password = getpass.getpass('Enter master password')
-    print("Pass fingerprint = %s" % get_master_password_fingerpring(master_password))
+def generate_site_pwd(master_password):
     print("version")
     ok = False
     while not ok:
@@ -79,6 +70,17 @@ def generate_site_pwd():
     login = sys.stdin.readline()
     print(calc_site_pwd(version, site, login, master_password))
 
+def generate_site_pwd_once():
+    master_password = getpass.getpass('Enter master password')
+    print("Pass fingerprint = %s" % get_master_password_fingerprint(master_password))
+    generate_site_pwd(master_password)
+
+def generate_site_pwd_many():
+    master_password = getpass.getpass('Enter master password')
+    print("Pass fingerprint = %s" % get_master_password_fingerprint(master_password))
+    Finished = False
+    while not Finished:
+        generate_site_pwd(master_password)
 
 def generate_random_pwd():
     """Generates random password.
@@ -92,7 +94,23 @@ def generate_random_pwd():
     i1 = random.randint(0, 31)
     s2 = os.urandom(i1 + 31)
     h2 = hashlib.sha1(h1 + s2).digest()
-    print(convert_from_bin_to_string(h2)[:24])
+    print(convert_from_bin_to_string(h2)[:20])
 
-generate_site_pwd()
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == '-r':
+        generate_random_pwd()
+    elif len(sys.argv) > 1 and sys.argv[1] == '-m':
+        print("Press Ctrl+C to exit")
+        try:
+            generate_site_pwd_many()
+        except KeyboardInterrupt:
+            print("\tExiting")
+            Finished = True
+    else:
+        try:
+            generate_site_pwd_once()
+        except KeyboardInterrupt:
+            print("\tExiting")
+            Finished = True
+
 
